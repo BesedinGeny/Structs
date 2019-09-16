32fe81d7a1fff71a99e32c695a88366588b11587
Eugen Besedin <besedin_geny@mail.ru>
2019-09-16 18:46:58 +0700

    One way list, base funcs

--- structs.h
+++ structs.h
@@ -3,7 +3,9 @@
 
 #endif // STRUCTS_H
 #define N 1000
+
 #include<iostream>
+
 using namespace std;
 
 class stack{
@@ -57,6 +59,7 @@ public:
         uS = -1;
         return 0;
     }
+
 };
 
 //круговая модель очереди
@@ -69,21 +72,7 @@ private:
 public:
 
     //без указания максимальной длины очереди
-    /*queue(){
-         mas = new int(N);
-    }
 
-    //с указанием макс. длинны очереди
-    queue(int SIZE){
-         mas = new int(SIZE);
-        S = SIZE;
-    }
-
-    //очищение памяти
-    ~queue(){
-        delete mas;
-    }
-*/
     //добавление в очередь
     int push(int elem){
         if (uL == (uR + 1) % S)
@@ -134,5 +123,86 @@ public:
     int clean(){
         uL = uR = -1;
         return 0;
+    }
+
+    //печать с удалением
+    int printQ(){
+        while (!this->isEmpty()){
+            int current;
+            current = this->pop();
+            cout << current << " ";
+        }
+        cout << endl;
+        return 0;
+    }
+};
+
+//односвязный список
+class list{
+
+
+private :
+    //информационное звено списка
+    struct Node{
+    public:
+        int Data;
+        Node *Next;
+    };
+    Node *Head = NULL;
+public:
+
+   //создание N элементов списка и заполнение нулями
+    int Create(int n )
+    {
+        for ( int i = 0; i < n; i++){
+            Node *Current = new Node;
+            if (Head == NULL){
+                Head = Current;
+                Current->Next = NULL;
+                Current->Data = 0;
+            }
+            else{
+                Current->Next = new Node;
+                Current = Current->Next;
+                Current->Next = NULL;
+                Current->Data = 0;
+            }
+        }
+
+        return 0;
+    }
+
+    //получить голову списка
+    Node *GetHead(){
+        return Head;
+    }
+
+    //вставить после n-ного элемента
+    int add(int n, int Data){
+        int i = 0;//считчик текущего элемента
+        //если элемента с таким номером не существует, нода будет добавлена в конец
+        Node *Current = Head;
+        while ((i < n)&&(Current->Next != NULL)){
+            i++;
+            Current = Current->Next;
+
+        }
+        //пришли к n-ному элементу, либо к последнему
+        Node *NewEl = new Node;
+        NewEl->Next = Current->Next;
+        NewEl->Data = Data;
+        Current->Next = NewEl;
+        return 0;
+
+    }
+
+    //очистка памяти листа
+    int Clear(){
+        Node *Current = Head;
+        while (Head->Next != NULL){
+               Current = Head;
+               Head = Head->Next;
+               delete Current;
+        }
     }
 };
